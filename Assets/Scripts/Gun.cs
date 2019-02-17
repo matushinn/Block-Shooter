2590f8910372e4fd942c5c24b8ca5f2ffbf4a31d
shotaro-ooe <shoooe97@gmail.com>
2019-02-17 15:54:32 +0900

    弾丸発射成功

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    //弾丸のプレハブ
+    public GameObject bulletPrefab;
+
+    //弾丸の初速
+    public float initialVelocity;
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown("space")){
+            //弾丸プレハブのインスタンス化
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+
+            //前方向への速度ベクトルを設定する
+            Vector3 direction = transform.forward;
+            bullet.GetComponent<Rigidbody>().velocity = direction * initialVelocity;
+
+        }
+		
+	}
+}
